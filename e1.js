a951209e809c9429efc6916dac4763cc49a39a05
Ghost5x9 <danieltaype68@outlook.com>
2024-01-19 11:45:09 -0500

    PC1

--- e1.js
+++ e1.js
@@ -8,29 +8,35 @@ function casillaOnClick(i, j) {
     console.log("Hizo Click en", i, j);
     
     casilla = devolverCasilla(i, j);
+    let casilla_espejo = devolverCasilla(15-i,15-j);
+    let casillaarri = devolverCasilla(i-1,j);
+    let casillaabajo = devolverCasilla(i+1,j);
+    let casilladera = devolverCasilla(i,j+1);
+    let casillaiz = devolverCasilla(i,j-1);
     
-    let casillaarri = (i-1,j);
-    let casillaabajo = (i+1,j);
-    let casilladera = (i,j+1);
-    let casillaiz = (i,j-1);
 
-    if(casillaarri){
-        casillaarri.innerHTML = "X";
+    if(casilla){
+        casilla.innerHTML = "X";;
+        if(casillaarri){
+            casillaarri.innerHTML = "X";
+        }
+
+        if(casillaabajo){
+            casillaabajo.innerHTML = "X";
+        }
+
+        if(casilladera){
+            casilladera.innerHTML = "X";      
+        }
+        
+        if(casillaiz){
+            casillaiz.innerHTML = "X";
+        }
+    }
+    else{
+        
     }
 
-    if(casillaabajo){
-        casillaabajo.innerHTML = "X";
-    }
-
-    if(casilladera){
-        casilladera.innerHTML = "X";
-    }
-    
-    if(casillaiz){
-        casillaiz.innerHTML = "X";
-    }
-    
-    casilla.disabled = true; 
 }
 
 
