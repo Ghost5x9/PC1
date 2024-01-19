2410ad15745c9e8574618b19db351cc907578923
Ghost5x9 <danieltaype68@outlook.com>
2024-01-19 11:19:49 -0500

    PC

---
+++ e1.js
@@ -0,0 +1,19 @@
+
+function devolverCasilla(i,j){
+    return document.getElementById(i+"_"+j)
+}
+let casilla=null
+
+function casillaOnClick(i, j) {
+    console.log("Hizo Click en", i, j);
+    
+    casilla = devolverCasilla(i, j);
+    
+    
+    casilla.innerHTML="X";
+    
+    casilla.disabled = true; 
+}
+
+
+main()
